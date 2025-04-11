abcf35df26d59e0429501a6a3db6d2d8ca949eed
hideyukiMORI <info@xion.cc>
2025-04-11 22:03:53 +0900

    update : 体力変化時にプログレスバーをアニメーションさせる

--- ui/hud.gd
+++ ui/hud.gd
@@ -6,6 +6,12 @@ extends Control
 @onready var health_bar = $TopBar/HealthBar
 @onready var health_label = $TopBar/HealthBar/HealthLabel
 
+var current_health: float = 0.0
+var target_health: float = 0.0
+var current_max_health: float = 0.0
+var target_max_health: float = 0.0
+var health_tween: Tween = null
+
 func _ready() -> void:
 	# グローバルなスコア変更シグナルを接続
 	GameState.score_changed.connect(_on_score_changed)
@@ -42,11 +48,36 @@ func update_health_display() -> void:
 	health_bar.value = PlayerStats.get_health()
 	health_label.text = "%d / %d" % [PlayerStats.get_health(), PlayerStats.get_max_health()]
 
-func _on_health_changed(_current: float, _max: float) -> void:
-	update_health_display()
+func _on_health_changed(current: float, max_health: float) -> void:
+	animate_health(current, max_health)
 
-func _on_max_health_changed(_new_max: float) -> void:
-	update_health_display()
+func _on_max_health_changed(new_max: float) -> void:
+	animate_health(PlayerStats.get_health(), new_max)
+
+func animate_health(new_health: float, new_max: float) -> void:
+	# 既存のTweenを停止
+	if health_tween:
+		health_tween.kill()
+	
+	# 新しいTweenを作成
+	health_tween = create_tween()
+	health_tween.set_trans(Tween.TRANS_CUBIC)
+	health_tween.set_ease(Tween.EASE_OUT)
+	
+	# 最大体力のアニメーション
+	health_tween.tween_property(health_bar, "max_value", new_max, 0.3)
+	
+	# 現在の体力のアニメーション
+	health_tween.parallel().tween_property(health_bar, "value", new_health, 0.3)
+	
+	# ラベルのアニメーション
+	health_tween.parallel().tween_method(
+		func(value: float):
+			health_label.text = "%d / %d" % [int(value), int(new_max)],
+		health_bar.value,
+		new_health,
+		0.3
+	)
 
 func get_health() -> float:
 	return health_bar.value
